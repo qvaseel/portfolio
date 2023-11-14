4c21945db6ea7b2c7ad79fcf57fa68342520d8aa
qvaseel <118725399+qvaseel@users.noreply.github.com>
2023-11-14 18:33:52 +0500

    add files

---
+++ script.js
@@ -0,0 +1,31 @@
+const slider = document.querySelector('.slider');
+const prevButton = document.querySelector('.prev-button');
+const nextButton = document.querySelector('.next-button');
+const slides = Array.from(slider.querySelectorAll('.slide'));
+const slideCount = slides.length;
+let slideIndex = 0;
+
+prevButton.addEventListener('click', showPreviousSlide);
+nextButton.addEventListener('click', showNextSlide);
+
+function showPreviousSlide() {
+  slideIndex = (slideIndex - 1 + slideCount) % slideCount;
+  updateSlider();
+}
+
+function showNextSlide() {
+  slideIndex = (slideIndex + 1) % slideCount;
+  updateSlider();
+}
+
+function updateSlider() {
+  slides.forEach((slide, index) => {
+    if (index === slideIndex) {
+        slide.classList.remove('none');
+    } else {
+        slide.classList.add('none');
+    }
+  });
+}
+
+updateSlider();
